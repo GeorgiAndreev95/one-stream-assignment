16274bc9319a440dbe9ad3ceace1bae2336f6c5a
Georgi Andreev <georgi.andreev995@gmail.com>
2025-07-31 00:40:42 +0300

    saved my API ket in a .env

--- axiosInstance.js
+++ axiosInstance.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
+const key = import.meta.env.VITE_TMDB_API_KEY;
+
 const axiosInstance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     headers: {
         accept: "application/json",
-        Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyY2NjYjQ1MWM2MDhlY2ZmYTgxMzkwZDRjNmI5ZjQ0NiIsIm5iZiI6MTc1Mzc4ODY0Ny44ODYwMDAyLCJzdWIiOiI2ODg4YjBlNzM5ZjIxOTY3YzVkOWJlMGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.exBT9g6jwzBjyhg8vpGDPBUXNbSGn2yzYgt8j2nO23U",
+        Authorization: `Bearer ${key}`,
     },
 });
 
